78219f78ab252e1ed7bb7ef32e3b53413f1f94d5
Edwin <w1947683@my.westminster.ac.uk>
2024-04-18 17:40:59 +0100

    Added first name, last name and email as fields to the registration form.

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model as user_model
+from django import forms
+
+User = user_model()
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def register(request):
     if request.method != 'POST':
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     else:
-        form = UserCreationForm(data=request.POST)
+        form = UserRegistrationForm(data=request.POST)
 
         if form.is_valid():
             new_user = form.save()
