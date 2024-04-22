10b161b39d1a611036c2979d52d9a72c03976e34
Ernesto Cosentino <cosentinoernesto0@gmail.com>
2024-04-22 13:24:00 +0100

    create a reports page

--- inventories/views.py
+++ inventories/views.py
@@ -5,3 +5,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     return render(request, 'inventories/index.html')
+
+def reports(request):
+    return render(request, "inventories/reports.html")
