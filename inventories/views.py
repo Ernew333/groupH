89aeaafdc138df92e49e096e9a23b2bbb2948c87
Azim <azimhameed4@gmail.com>
2024-04-23 12:28:28 +0100

    All functions of manageitem fully working will add styles

--- inventories/views.py
+++ inventories/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Item
+from .models import Item 
 from .forms import ItemForm
 
 
