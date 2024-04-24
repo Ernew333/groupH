19f3ce691940f2e4a69a7b2d3bc5a6ba4538ac6d
Edwin <w1947683@my.westminster.ac.uk>
2024-04-24 16:55:37 +0100

    Added dynamic label and count for sidebar filter.

--- inventories/views.py
+++ inventories/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Item
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'inventories/index.html')
+    items = Item.objects.all()
+
+    types = getTypes(items)
+    statuses = getStatuses(items)
+
+    context = {'items': items, 'types': types, 'statuses': statuses}
+    return render(request, 'inventories/index.html', context)
+
+def getTypes(items):
+    types = {}
+
+    for item in items:
+        types[item.type] = countType(items, item.type)
+
+    return types
+
+def getStatuses(items):
+    statuses = {}
+
+    for item in items:
+        statuses[item.status] = countStatus(items, item.status)
+
+    return statuses
+
+def countType(items, selectedType):
+    return items.filter(type=selectedType).count()
+
+def countStatus(items, selectedStatus):
+    return items.filter(status=selectedStatus).count()
