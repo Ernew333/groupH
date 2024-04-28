044e1b308e7b93ab98541e7b5e2691e1ad120e2c
Ozair <ozair0014@gmail.com>
2024-04-28 18:06:54 +0100

    Minor changes

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from .forms import UserRegistrationForm
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
