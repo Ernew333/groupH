e21d661f6133c913afd544018a8057effbf90d4b
Azim <azimhameed4@gmail.com>
2024-04-29 21:39:00 +0100

    Comments added to pages

--- inventories/urls.py
+++ inventories/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 app_name = 'inventories'
 
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('manageitem/', views.manageItem, name = "manageitem"),
-    path('createitem/', views.createItem, name = "createitem"),
-    path('updateitem/<str:id>/', views.updateItem, name = "updateitem"),
-    path('deleteitem/<str:id>/', views.deleteItem, name = "deleteitem"),
+urlpatterns = [#all of the URL Patterns for the project
+    path('', views.index, name="index"),#URL pattern for the index page
+    path('manageitem/', views.manageItem, name = "manageitem"),#URL pattern for the manage item page
+    path('createitem/', views.createItem, name = "createitem"),#URL pattern for the create item page
+    path('updateitem/<str:id>/', views.updateItem, name = "updateitem"),#URL pattern for the update item page
+    path('deleteitem/<str:id>/', views.deleteItem, name = "deleteitem"),#URL pattern for the delete item page
 ]
--- inventories/views.py
+++ inventories/views.py
@@ -7,41 +7,41 @@ from .forms import ItemForm
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'inventories/index.html')
+    return render(request, 'inventories/index.html')#renders the index page
 
 def manageItem(request):
-    item = Item.objects.all()
-    context = {'item': item}
-    return render(request,'inventories/manageitem.html', context)
+    item = Item.objects.all()#retrieves all items from the database
+    context = {'item': item}#the items are passed onto the template  
+    return render(request,'inventories/manageitem.html', context)#render the manage item page items retrieved from the database
 
 def createItem(request):
     form = ItemForm()
 
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
-        if form.is_valid():
+    if request.method == 'POST':#checks if the method is post
+        form = ItemForm(request.POST)#the form instance stores the data from the POST
+        if form.is_valid():#check if the form is valid and if it is the data will get saved to database
             form.save()
-            return redirect('inventories:manageitem')
-    context = {'form': form}
-    return render(request, 'inventories/item_form.html', context)
+            return redirect('inventories:manageitem')# redirects back to manage item page 
+    context = {'form': form}#form passed into the template 
+    return render(request, 'inventories/item_form.html', context)#renders the form page
 
 def updateItem(request,id):
-    item = Item.objects.get(id=id)
-    form = ItemForm(instance=item)
+    item = Item.objects.get(id=id)#retrieves item by its id
+    form = ItemForm(instance=item)#instance of the form is created stored the item data
 
-    if request.method == 'POST':
-        form = ItemForm(request.POST, instance=item)
-        if form.is_valid():
+    if request.method == 'POST':#checks if the method is post
+        form = ItemForm(request.POST, instance=item)#the form instance stores the data from the POST
+        if form.is_valid():#check if the form is valid and if it is the data will get saved to database
             form.save()
-            return redirect('inventories:manageitem')
+            return redirect('inventories:manageitem')# redirects back to manage item page 
 
     context = {'form': form}
-    return render(request, 'inventories/item_form.html', context)
+    return render(request, 'inventories/item_form.html', context)#renders the form page
 
 def deleteItem(request, id):
-    item = Item.objects.get(id=id)
-    context = {'item': item}
-    if request.method == 'POST':
-        item.delete()
-        return redirect('inventories:manageitem')
-    return render(request, 'inventories/delete.html', {'item': item })
+    item = Item.objects.get(id=id)#retrieves item by its id
+    context = {'item': item}#the items are passed onto the template 
+    if request.method == 'POST':#checks if the method is post
+        item.delete()#item will be deleted
+        return redirect('inventories:manageitem')# redirects back to the manage item page 
+    return render(request, 'inventories/delete.html', {'item': item })#renders the delete page which asks if you really want to delete. 
