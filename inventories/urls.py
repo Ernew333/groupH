a4cac396597b65f6caf5904d282ed4344c307e80
Azim <azimhameed4@gmail.com>
2024-04-29 21:59:32 +0100

    Added comments to the pages

--- inventories/urls.py
+++ inventories/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 app_name = 'inventories'
 
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('booking/', views.viewBooking, name = "viewbooking"),
-    path('cancel_booking/<str:id>/', views.cancelBooking, name = "cancelbooking"),
+urlpatterns = [#all URL patterns used
+    path('', views.index, name="index"),#index page URL pattern
+    path('booking/', views.viewBooking, name = "viewbooking"),# view booking URL pattern
+    path('cancel_booking/<str:id>/', views.cancelBooking, name = "cancelbooking"),#cancel booking URL pattern
 ]
--- inventories/views.py
+++ inventories/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
-from .models import Item , Booking
-from datetime import date
+from .models import Item , Booking#importing models needed for page 
+from datetime import date#importing date
 
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'inventories/index.html')
+    return render(request, 'inventories/index.html')#renders the index page
 
 def viewBooking(request):
-    completed_bookings = Booking.objects.filter(end_date__lt=date.today())
-    for cb in completed_bookings:
-        cb.status = "Completed"
+    completed_bookings = Booking.objects.filter(end_date__lt=date.today())#checks if booking is completed by checking if its past current date
+    for cb in completed_bookings:#for loop iterates completed_bookings
+        cb.status = "Completed"#status changed to completed and saves it
         cb.save()
-    booking = Booking.objects.all()
-    return render(request, 'inventories/bookings.html',{'booking': booking })
+    booking = Booking.objects.all()#retrieve all bookings from database
+    return render(request, 'inventories/bookings.html',{'booking': booking })#booking page is rendered with all booking retrived from database
 
 def cancelBooking(request,id):
-    cancel_booking = Booking.objects.get(id=id)
-    cancel_booking.status = "Cancelled"
+    cancel_booking = Booking.objects.get(id=id)#retrieves id of booking your trying to cancel
+    cancel_booking.status = "Cancelled"#changes the status to cancelled and saves it 
     cancel_booking.save()
-    return redirect('inventories:viewbooking')
+    return redirect('inventories:viewbooking')#redirects you back to view booking page
     
