4a4cd57863860fbff44f8489297fa33810420d4a
Ozair <ozair0014@gmail.com>
2024-04-29 20:41:23 +0100

    Implemented edit page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model as user_model
+from django.contrib.auth.forms import PasswordChangeForm
 from django import forms
 
 User = user_model()
