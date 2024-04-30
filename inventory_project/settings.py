d1868d9b1e01505f6c0f14911f53f8cb43cb5d48
Ozair <ozair0014@gmail.com>
2024-04-30 14:27:26 +0100

    Minor Changes

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -136,5 +137,5 @@ LOGIN_REDIRECT_URL = 'inventories:index'
 LOGOUT_REDIRECT_URL = 'inventories:index'
 LOGIN_URL = 'accounts:login'
 
-MEDIA_ROOT = ''
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
